21e4d6ed700d64507be47c940fd9b3afff86a672
VitaliiVlo <vitalii.vlo@gmail.com>
2018-06-28 13:52:37 +0300

    refactor 'favourite' controller

--- controllers/favourite.js
+++ controllers/favourite.js
@@ -1,87 +1,63 @@
 const express = require('express'),
     router = express.Router(),
-    db = require("../lib/db")
+    logger = require('../lib/log')(module),
+    Song = require('../models/song').Song,
+    User = require('../models/user').User;
+
 
 router.get("/", function (req, res) {
     res.render("favourite");
-})
+});
 
 // ajax route to add new favourite song (main page)
-router.get("/add/:id", function (req, res) {
-    song = req.params.id;
-    user = req.session.user;
+router.get("/add/:song_id", function (req, res) {
+    var song_id = req.params.song_id;
+    var user_id = req.session.user;
 
-    db.addFavor(user, song, function (err, result) {
+    User.findById(user_id).populate('songs').exec(function (err, user) {
         if (err) {
             res.status(400);
-            console.log(err);
-            return;
-        } else {
-            res.status(200);
+            logger.error(err);
         }
-    })
-})
+        user.songs.push(song_id);
+        user.save(function (err) {
+            if (err) {
+                res.status(400);
+                logger.error(err);
+            } else {
+                res.status(200);
+            }
+        });
+    });
+});
 
 // select info about each song of user (favourite page)
 router.get("/list", function (req, res) {
-    user = req.session.user;
+    var user_id = req.session.user;
 
-    db.getSongsByUser(user, function (err, result) {
+    User.findById(user_id).populate('songs').exec(function (err, user) {
         if (err) {
             res.status(400);
-            console.log(err);
-            return;
+            logger.error(err);
         } else {
             res.status(200).send({
-                "playlist": result
+                "playlist": user.songs
             });
         }
-    })
-})
+    });
+});
 
 // route to delete song from favourite list
-router.get("/delete/:id", function (req, res) {
-    song = req.params.id;
-    user = req.session.user;
-
-    db.deleteFavor(user, song, function (err, result) {
+router.get("/delete/:user_id", function (req, res) {
+    var song_id = req.params.user_id;
+    User.findByIdAndUpdate(req.session.user, {$pull: {songs: song_id}}).exec(function (err) {
         if (err) {
             res.status(400);
-            console.log(err);
-            return;
+            logger.error(err);
         } else {
             res.status(200);
         }
-    })
-})
+    });
+});
 
-
-// check song existing in favoutite list
-router.get("/check/:id", function (req, res) {
-    song = req.params.id;
-    user = req.session.user;
-
-    db.getSongByUserAndSong(user, song, function (err, result) {
-        if (err) {
-            res.status(400);
-            console.log(err);
-            return;
-        } else {
-
-            var flag;
-            if (result.length == 1) {
-                flag = "true";
-            } else if (result.length == 0) {
-                flag = "false";
-            }
-
-            res.status(200).send({
-                "flag": flag
-            });
-
-        }
-    })
-})
-
-
-module.exports = router
+module.exports = router;
--- public/js/favourite.js
+++ public/js/favourite.js
@@ -1,58 +1,52 @@
-var g;
+var songs;
 
-$(document).ready(function(){
+$(document).ready(function () {
+    $.ajax({
+        url: "/favourite/list",
+        success: function (data) {
+            songs = data.playlist;
 
-	$.ajax({
-		url: "/favourite/list",
-		success: function(data) {
+            var s = "";
+            var d = "";
+            for (var k = 0; k < songs.length; k++) {
+                s = s + "<li onclick=lyrics(" + k + "); id='w" + k + "' class='list-group-item list-group-item-success'>" + songs[k].name + "</li>";
+                d = d + "<li id='d" + k + "' onclick=del(" + k + "); class='list-group-item list-group-item-danger'>&times;</li>"
+            }
 
-			g = data;
+            $("#songlist").html(s);
+            $("#deletelist").html(d);
+        }
+    })
+});
 
-			var s = ""
-			var d = ""
+function lyrics(k) {
+    $("#words").html(songs[k].lyrics);
+    var player = $("#player");
+    player.attr("src", songs[k].audio);
+    player.hide();
+    player.css("width", "0");
 
-			for (var k = 0; k < data.playlist.length; k++) {
-				s = s + "<li onclick=words(" + k + "); id='w"+k+"' class='list-group-item list-group-item-success'>" + data.playlist[k].songname + "</li>"
-				d = d + "<li id='d"+k+"' onclick=del(" + k + "); class='list-group-item list-group-item-danger'>&times;</li>"
-			}
-			
-			$("#songlist").html(s);
-			$("#deletelist").html(d);
-
-		}
-	})
-
-})
-
-function words(k){
-	$("#words").html(g.playlist[k].words);
-	var varforsong = g.playlist[k].songname;
-	varforsong = varforsong.toLowerCase();
-	varforsong = varforsong.replace(/\s+/g,"");
-	$("#player").attr("src","public/audio/"+varforsong+".mp3");
-
-	$("#player").hide();
-	$("#player").css("width","0");
-	$("#aud").show();
-	$("#myModal").modal();
+    $("#aud").show();
+    $("#myModal").modal();
 }
 
-function play(){
-	$("#aud").hide();
-	$("#player").show();
-	$('#player').animate({
-		width : "90%"
-	});
+function play() {
+    $("#aud").hide();
+    var player = $("#player");
+    player.show();
+    player.animate({
+        width: "90%"
+    });
 }
 
-function del(k){
-	var w = "w"+k;
-	var d = "d"+k;
+function del(k) {
+    var w = "w" + k;
+    var d = "d" + k;
 
-	var id = g.playlist[k].song_id;
+    var id = songs[k]._id;
 
-	$("#"+w).hide();
-	$("#"+d).hide();
+    $("#" + w).hide();
+    $("#" + d).hide();
 
-	$.ajax({url: "/favourite/delete/"+id})
+    $.ajax({url: "/favourite/delete/" + id})
 }
